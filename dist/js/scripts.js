7c8f3cf9aa2df211905640dff217febfab9becd0
Paulo Roberto <evans_sp@hotmail.com>
2021-03-26 10:19:47 -0300

    add alert

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -43,3 +43,5 @@ jQuery(function(){
         jQuery('#contagem_visualizou_semclique_ofertas').text(++_nbr);//Increment variable and puts it in the element
     },500);//Number of ms between function calls
 });
+
+$('.alert').alert()
