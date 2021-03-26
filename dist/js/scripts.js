68f8d05604a79abd5a783592e8b1f914dce72007
Paulo Roberto <evans_sp@hotmail.com>
2021-03-26 13:35:38 -0300

    edit modal

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -74,4 +74,12 @@ $(document).ready(function(){
     });
 
 
-});  
+}); 
+
+
+//Frases
+
+var typed = new Typed('.element', {
+    strings: ['This is a JavaScript library', 'This is an ES6 module'],
+    smartBackspace: true // Default value
+  });
