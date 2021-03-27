a31027df63a4b709e200fe92e317ba1dfb689966
Paulo Roberto <evans_sp@hotmail.com>
2021-03-26 21:41:38 -0300

    Typed Funcionando

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -3,19 +3,19 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
     */
-    (function($) {
+(function ($) {
     "use strict";
 
     // Add active state to sidbar nav links
     var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
-        $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
-            if (this.href === path) {
-                $(this).addClass("active");
-            }
-        });
+    $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function () {
+        if (this.href === path) {
+            $(this).addClass("active");
+        }
+    });
 
     // Toggle the side navigation
-    $("#sidebarToggle").on("click", function(e) {
+    $("#sidebarToggle").on("click", function (e) {
         e.preventDefault();
         $("body").toggleClass("sb-sidenav-toggled");
     });
@@ -23,38 +23,38 @@
 
 
 
-jQuery(function(){    
-    setInterval(function(){ //Calls a function every X ms as specified in param 2
+jQuery(function () {
+    setInterval(function () { //Calls a function every X ms as specified in param 2
         var _nbr = parseInt(jQuery('#contagem_visualizacoes').text());//Get number from element as int
         jQuery('#contagem_visualizacoes').text(++_nbr);//Increment variable and puts it in the element
-    },500);//Number of ms between function calls
+    }, 500);//Number of ms between function calls
 });
 
-jQuery(function(){    
-    setInterval(function(){ //Calls a function every X ms as specified in param 2
+jQuery(function () {
+    setInterval(function () { //Calls a function every X ms as specified in param 2
         var _nbr = parseInt(jQuery('#contagem_clique_ofertas').text());//Get number from element as int
         jQuery('#contagem_clique_ofertas').text(++_nbr);//Increment variable and puts it in the element
-    },500);//Number of ms between function calls
+    }, 500);//Number of ms between function calls
 });
 
-jQuery(function(){    
-    setInterval(function(){ //Calls a function every X ms as specified in param 2
+jQuery(function () {
+    setInterval(function () { //Calls a function every X ms as specified in param 2
         var _nbr = parseInt(jQuery('#contagem_visualizou_semclique_ofertas').text());//Get number from element as int
         jQuery('#contagem_visualizou_semclique_ofertas').text(++_nbr);//Increment variable and puts it in the element
-    },500);//Number of ms between function calls
+    }, 500);//Number of ms between function calls
 });
 
 
 //Delete linhas
 
-$(document).ready(function() {
-    $("#dataTable .deleteLink").on("click",function() {
+$(document).ready(function () {
+    $("#dataTable .deleteLink").on("click", function () {
         var td = $(this).parent();
         var tr = td.parent();
         //change the background color to red before removing
-        tr.css("background-color","#FF3700");
+        tr.css("background-color", "#FF3700");
 
-        tr.fadeOut(400, function(){
+        tr.fadeOut(400, function () {
             tr.remove();
         });
     });
@@ -63,16 +63,14 @@ $(document).ready(function() {
 
 //Cadastrar novas ofertas
 
-$(document).ready(function(){
-    $(".add").click(function(){
+$(document).ready(function () {
+    $(".add").click(function () {
         var idoferta = $("#idoferta").val();
         var nomeoferta = $("#nomeoferta").val();
         var descontooferta = $("#descontooferta").val();
         var ativodesde = $("#ativodesde").val();
-        var markup = "<tr><td>" + idoferta + "</td><td>" + nomeoferta + "</td><td>" + descontooferta +"%"+  "</td><td></td><td>" + ativodesde + "</td><td></td></tr>";
+        var markup = "<tr><td>" + idoferta + "</td><td>" + nomeoferta + "</td><td>" + descontooferta + "%" + "</td><td></td><td>" + ativodesde + "</td><td></td></tr>";
         $("#dataTable").append(markup);
     });
 
-
-}); 
-
+});
